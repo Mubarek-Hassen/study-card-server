d7d27e4b6cf52539723e23199bac2126977dc0ed
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-03-11 21:00:14 -0500

    db connection written

---
+++ config/db.connection.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// const connectionStr = process.env.MONGODB_URI
+
+mongoose.set('strictQuery', true)
+
+// mongoose.connect(connectionStr)
+
+mongoose.connection.on('connected', ()=>console.log(`Connected!`))
+mongoose.connection.on('error', ()=>console.log(`error ===>>> ${error}`))
+mongoose.connection.on('disconnected', ()=>console.log(`Mongo Disconnected!`))
