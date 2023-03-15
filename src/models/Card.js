58cf185a9feacec8c8f514f9dc4f4749d10297ba
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-03-14 21:22:30 -0400

    card model added

--- src/models/Card.js
+++ src/models/Card.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 
-// const cardSchema = mongoose.Schema({
-//   title: {
-
-//   }
-// })
+const cardSchema = mongoose.Schema({
+  card: {
+    front: {
+      Type: String,
+      required: [true, "Please add description."]
+    },
+    back: {
+      Type: String,
+      required: [true, "Please add word."]
+    },
+  }
+},{
+  timestamps: true
+})
