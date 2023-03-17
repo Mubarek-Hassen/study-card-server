e99ca0d7941c38610a1cd3595159eae1c643d59d
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-03-16 21:49:04 -0400

    route started, need controllers

---
+++ src/routes/cards.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+
+// const Card = require('../models/Card')
+
+const {} = require('../controllers/cards-controller')
+
+// router.route('/').get(protect, getCards).post(protect, createCard)
