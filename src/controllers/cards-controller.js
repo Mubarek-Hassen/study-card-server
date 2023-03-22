cb082000ca867a829053c1fdfee287b42db9caf4
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-03-21 21:06:44 -0400

    working on post method

--- src/controllers/cards-controller.js
+++ src/controllers/cards-controller.js
@@ -1,10 +1,22 @@
 const Card = require('../models/Card')
+const mongoose = require('mongoose')
 
 const getCards = async(req,res)=>{
     const cards = await Card.find({})
     res.json(cards)
   }
 
+  const createCard = async(req, res)=>{
+    const {front, back} = req.body
+    try {
+      const createdCard = await Card.create( front, back)
+      res.json({msg: "Card Created!"})
+    } catch (error) {
+      res.status(400).json({error: 'Unable to add card.'})
+    }
+  }
+
 module.exports = {
   getCards,
+  createCard,
 }
--- src/routes/cards.js
+++ src/routes/cards.js
@@ -5,9 +5,11 @@ const Card = require('../models/Card')
 
 const { 
   getCards,
+  createCard,
 } = require('../controllers/cards-controller')
 
 router.get('/', getCards)
+router.post('/new-card', createCard)
 
 
 module.exports = router
