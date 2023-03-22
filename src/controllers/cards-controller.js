4e1f9aea45e045e21600cf897aec4f2bffc34bc5
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-03-21 21:08:42 -0400

    card create fixed

--- src/controllers/cards-controller.js
+++ src/controllers/cards-controller.js
@@ -9,7 +9,7 @@ const getCards = async(req,res)=>{
   const createCard = async(req, res)=>{
     const {front, back} = req.body
     try {
-      const createdCard = await Card.create( front, back)
+      const createdCard = await Card.create({front, back})
       res.json({msg: "Card Created!"})
     } catch (error) {
       res.status(400).json({error: 'Unable to add card.'})
