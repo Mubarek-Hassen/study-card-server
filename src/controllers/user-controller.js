aeb772b94084d83f7ecbbb338d33908725066490
Mubarek-Hassen <Mubarekhassen4@gmail.com>
2023-03-28 21:23:31 -0400

    working on login user controller, half way

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -13,9 +13,9 @@ const registerUser = async (req,res)=>{
     return res.json({message: "User already exists!"})
   }
 
-  // const salt = await bcrypt.genSalt(10);
+  const salt = await bcrypt.genSalt(10);
   
-  const hashedPassword = await bcrypt.hash(password, 10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
   const newUser = await userModel.create({ email, name, password: hashedPassword})
 
@@ -25,9 +25,29 @@ const registerUser = async (req,res)=>{
 
 // LOGIN USER
 
+const loginUser = async (req, res) => {
+  const { email, name, password} = req.body;
+
+  const user = await userModel.findOne({ email });
+
+  if(!user){
+    return res.json({message: "User doesn't exist!"})
+  }
+
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+
+  if(!isPasswordValid){
+    return res.json({message: "Email or password is incorrect!"})
+  }
+
+  const token = jwt.sign({id: user._id}, "secret")
+
+}
+
 
 
 // EXPORT ALL USER CONTROLLING FUNCTIONS
 module.exports = {
   registerUser,
+  loginUser,
 }
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,10 +1,10 @@
 const express = require('express')
 
-const { registerUser } = require('../controllers/user-controller.js')
+const { registerUser, loginUser } = require('../controllers/user-controller.js')
 
 const router = express.Router()
 
 router.post("/register", registerUser)
-// router.post("/login", loginUser)
+router.post("/login", loginUser)
 
 module.exports = router
